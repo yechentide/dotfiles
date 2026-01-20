591d1b8b061d45ce3dae8b85a8aa5b09ac9a6dd8
yechentide <yechentide@gmail.com>
2026-01-20 21:43:44 +0900

    feat: enable starship prompt with custom theme and improve manual for Debian/Ubuntu

--- manuals/oracle-ubuntu-init.sh
+++ manuals/oracle-ubuntu-init.sh
@@ -4,45 +4,64 @@ set -eu
 
 # sudo hostnamectl set-hostname xxx
 sudo timedatectl set-timezone Asia/Tokyo
-sudo apt update && sudo apt upgrade -y && sudo apt install -y zsh tree zip unzip
+sudo apt update && sudo apt upgrade -y && sudo apt install -y zsh git unzip tmux htop tree
 sudo passwd
-sudo passwd ubuntu
-sudo chsh -s "$(which zsh)" ubuntu
-sudo chown -R ubuntu:ubuntu /usr/local/bin
-git clone --recursive https://github.com/yechentide/dotfiles.git ~/dotfiles
+sudo passwd "$USER"
+sudo chsh -s "$(which zsh)" "$USER"
+sudo chown -R "$USER:$USER" /usr/local/bin
+git clone --recursive https://github.com/yechentide/dotfiles.git "$HOME/dotfiles"
 ~/dotfiles/02_setup_xdg_env.sh
 exit
 
 rm .bash* .profile
 ~/dotfiles/04_install_zinit.sh
 vim "$XDG_CONFIG_HOME/zsh/.zshrc"
+ln -s "$HOME/dotfiles/config/zsh/starship-remote.toml" "$XDG_CONFIG_HOME/starship.toml"
 exit
 
-zinit self-update
+zinit self-update && zinit update
 ~/dotfiles/05_setup_config.sh
+exit
+
+OS="$(cat /etc/os-release | grep '^ID' | awk -F= '{print $2}')"
+declare -r OS
 
 # Docker
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker "$USER"
-docker --version
+if [[ "$OS" == 'debian' || "$OS" == 'ubuntu' ]]; then
+  sudo apt-get install ca-certificates curl gnupg lsb-release
 
-# Network
-# sudo iptables -D INPUT 5
-sudo iptables -L INPUT --line-numbers -n
-sudo iptables -I INPUT 5 -p tcp --dport 80 -j ACCEPT
-sudo iptables -I INPUT 6 -p tcp --dport 443 -j ACCEPT
-sudo iptables -I INPUT 7 -p tcp --dport 8080 -j ACCEPT
-sudo netfilter-persistent save
-sudo vim /etc/iptables/rules.v4
-sudo iptables -F
-sudo netfilter-persistent reload
-sudo iptables -L INPUT --line-numbers -n
+  if [[ "$OS" == 'debian' ]]; then
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  fi
+
+  if [[ "$OS" == 'ubuntu' ]]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  fi
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+  sudo groupadd docker
+  sudo usermod -aG docker "$USER"
+  docker --version
+fi
+
+# Network (Oracle Cloud)
+if [[ "$OS" == 'ubuntu' ]]; then
+  # sudo iptables -D INPUT 5
+  sudo iptables -L INPUT --line-numbers -n
+  sudo iptables -I INPUT 5 -p tcp --dport 80 -j ACCEPT
+  sudo iptables -I INPUT 6 -p tcp --dport 443 -j ACCEPT
+  sudo iptables -I INPUT 7 -p tcp --dport 8080 -j ACCEPT
+  sudo netfilter-persistent save
+  sudo vim /etc/iptables/rules.v4
+  sudo iptables -F
+  sudo netfilter-persistent reload
+  sudo iptables -L INPUT --line-numbers -n
+fi
