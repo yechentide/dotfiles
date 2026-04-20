d60533ca254e454597757ce13c2e7f960455ea71
yechentide <yechentide@gmail.com>
2026-04-21 06:13:45 +0900

    refactor: reorganize environment variables

--- config/zsh/exports.sh
+++ config/zsh/exports.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
+export ZSHRC="$ZDOTDIR/.zshrc"
 export LESSHISTFILE=$XDG_CACHE_HOME/lesshst
-
-export GOPATH="$XDG_DATA_HOME/go"
-export PATH=$GOPATH/bin:$PATH
-
-export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 
 export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
 
-export ZSHRC="$ZDOTDIR/.zshrc"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+export GOPATH="$XDG_DATA_HOME/go"
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
 
-export CODEX_HOME="$XDG_DATA_HOME/codex"
+export PATH=$GOPATH/bin:$PATH
